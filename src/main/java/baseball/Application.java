2dcee3e8ae9adc78aeed7d3b346ac7980fe1ac9e
LTJ <cn3085@gmail.com>
2022-10-03 21:52:50 +0900

    refactor(Application): 초기화 메소드 호출 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,9 +4,9 @@ import baseball.control.BaseballGame;
 import baseball.control.Game;
 
 public class Application {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         Game baseballGame = new BaseballGame();
-
+        baseballGame.init();
         while (baseballGame.wantPlayingGame()) {
             baseballGame.play();
         }
