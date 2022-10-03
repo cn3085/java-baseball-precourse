68c72d7ea1ade466bab0e84fba1bebdc85fa9117
LTJ <cn3085@gmail.com>
2022-10-03 15:26:13 +0900

    feat(Application): 게임 실행 로직 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.control.BaseballGame;
+import baseball.control.Game;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) { 
+        Game baseballGame = new BaseballGame();
+
+        while (baseballGame.wantPlayingGame()) {
+            baseballGame.play();
+        }
+        baseballGame.quit();
     }
 }
