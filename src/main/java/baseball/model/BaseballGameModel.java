a4b86569d0c0b3e4427740e099dd2f1a87e481c5
LTJ <cn3085@gmail.com>
2022-10-03 15:36:03 +0900

    feat(model): 야구게임 모델 추가

---
+++ src/main/java/baseball/model/BaseballGameModel.java
@@ -0,0 +1,38 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BaseballGameModel {
+
+    private Integer[] ballNumbers;
+    private boolean notYetCollectAnswer = true;
+    private boolean wantNewGame = true;
+
+    public BaseballGameModel() {
+        setNewGame();
+    }
+
+    public void setNewGame(){
+        this.notYetCollectAnswer = true;
+        this.wantNewGame = true;
+        this.ballNumbers = pickNewBallNumbers();
+    }
+
+    private Integer[] pickNewBallNumbers() {
+        Set<Integer> ballNumberSet = new HashSet<>();
+        while (ballNumberSet.size() < 3) {
+            ballNumberSet.add(Randoms.pickNumberInRange(0, 9));
+        }
+        return ballNumberSet.toArray(new Integer[3]);
+    }
+
+    public boolean getNotYetCollectAnswer(){
+        return notYetCollectAnswer;
+    }
+
+    public boolean getWantNewGame(){
+        return wantNewGame;
+    }
+}
