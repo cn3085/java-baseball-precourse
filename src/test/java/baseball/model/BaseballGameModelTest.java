32896326c678835863c5315a28bd26b3c53dec83
LTJ <cn3085@gmail.com>
2022-10-03 16:31:29 +0900

    test(model): 야구게임 모델 테스트 케이스 추가
    생성한 세개의 숫자가 1~9범위 내에서만 추가되는지 테스트 추가

--- src/test/java/baseball/model/BaseballGameModelTest.java
+++ src/test/java/baseball/model/BaseballGameModelTest.java
@@ -6,33 +6,42 @@ import java.util.HashSet;
 import java.util.Set;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class BaseballGameModelTest {
 
     @Nested
-    class 새게임_세팅_테스트{
+    class 새게임_세팅_테스트 {
 
         BaseballGameModel baseballGameModel = new BaseballGameModel();
 
         @Test
-        void 초기화시_새게임속성_참(){
+        void 초기화시_새게임속성_참() {
             Assertions.assertThat(baseballGameModel.getWantNewGame()).isTrue();
         }
 
         @Test
-        void 초기화시_아직정답못맞힘_참(){
+        void 초기화시_아직정답못맞힘_참() {
             Assertions.assertThat(baseballGameModel.getNotYetCollectAnswer()).isTrue();
         }
 
-        @Test
-        void 초기화시_유니크한세개의숫자(){
+        @RepeatedTest(20)
+        void 초기화시_유니크한세개의숫자() {
             //when
             Set<Integer> numberSet = new HashSet<>(Arrays.asList(baseballGameModel.getBallNumbers()));
 
             //then
             Assertions.assertThat(baseballGameModel.getBallNumbers().length).isEqualTo(3);
-            Assertions.assertThat(numberSet.size()).isEqualTo(4);
+            Assertions.assertThat(numberSet.size()).isEqualTo(3);
+        }
+
+        @RepeatedTest(20)
+        void 초기화시_숫자범위() {
+            //then
+            Assertions.assertThat(baseballGameModel.getBallNumbers())
+                    .filteredOn(num -> num > 0 && num < 10)
+                    .hasSize(3);
         }
     }
 }
